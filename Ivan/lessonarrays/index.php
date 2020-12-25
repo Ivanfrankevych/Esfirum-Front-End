808afd099bf48e5270ec98eb9c1002b8aed3d8bf
Ivanfrankevych <32160039+Ivanfrankevych@users.noreply.github.com>
2020-12-25 14:06:27 +0200

    asd

---
+++ Ivan/lessonarrays/index.php
@@ -0,0 +1,38 @@
+<?php include 'class.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+//    $myarr=array(10,'ddd',15);
+//    $a=array('first'=>'element','block','third'=>'position','fourth'=>'block');
+//    $b=array('first'=>'apple','vegetable'=>'element','third'=>'watermelon');
+//    $c=array_combine($a,$b);
+//    $d=array_diff_key($a,$b);
+//    $e=array_diff($a,$b);
+//    $f=array_pop($a);
+//    array_push($a,'orange','banana');
+//    $g=array_search('block',$a);
+//    $h=array_reverse($a);
+//    $i=array_search('block',$h);
+//    asort($a);
+//    $current=next($a);
+//    $current=next($a);
+//    $current=next($a);
+//    $current=prev($a);
+//    $current=reset($a);
+//    $sum=array_sum($myarr);
+//    $current=currrent($a);
+
+//    print_r($sum);
+    $my_square=new Square(5);
+    $my_square->PrintInfo();
+    ?>
+</body>
+
+</html>
